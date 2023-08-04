a128c1fea0a0d8bd4572f28c91c10bc98530d01b
Luka Bulatovic <luka.bulatovic@condor-business.com>
2023-08-04 15:01:33 +0200

    herb eater complete

--- src/engine/game/main.js
+++ src/engine/game/main.js
@@ -16,60 +16,171 @@ export class GameInstance {
 
     this.initialPlants = initObjects;
 
+    this.colorShades = [
+      "#ffffff",
+      "#fff2f2",
+      "#ffe6e6",
+      "#ffdbdb",
+      "#ffd0d0",
+      "#ffc4c4",
+      "#ffb9b9",
+      "#ffadad",
+      "#ffa2a2",
+      "#ff9696",
+      "#ff8b8b",
+      "#ff8080",
+      "#ff7474",
+      "#ff6969",
+      "#ff5d5d",
+      "#ff5252",
+      "#ff4747",
+      "#ff3b3b",
+      "#ff3030",
+      "#ff2424",
+      "#ff1919",
+      "#ff0d0d",
+      "#ff0202",
+      "#ff0000",
+      "#f20000",
+      "#e60000",
+      "#db0000",
+      "#d00000",
+      "#c40000",
+      "#b90000",
+      "#ad0000",
+    ];
+
+    console.log("SHADE COUNT: ", this.colorShades.length);
     this.canvas = canvas;
     this.initialize(this);
   }
 
   initialize(instance) {
-    this.canvas.addEventListener("mousedown", function (e) {
+    this.canvas.addEventListener("mousedown", async function (e) {
       let position = getMousePosition(instance.context.canvas, e);
 
-      let newObject = new GameObject({
-        gameProperties: "herbEater",
-        physics: {
-          graphicalObject: {
-            position: { x: position.x, y: position.y },
-            context: instance.context,
-            assetObject: null,
-            mathematicalObject: true,
-            color: "red",
-            radius: 20,
+      if (position.button == 0) {
+        let newObject = new GameObject({
+          gameProperties: {
+            eaten: 0,
+            shade: 0,
           },
-          physicalProperty: {
-            speed: 15,
-            target: {
-              x: null,
-              y: null,
-              object: null,
+          physics: {
+            graphicalObject: {
+              position: { x: position.x, y: position.y },
+              context: instance.context,
+              assetObject: null,
+              mathematicalObject: true,
+              color: "#ffffff",
+              radius: 20,
+            },
+            physicalProperty: {
+              speed: 5,
+              target: {
+                x: null,
+                y: null,
+                object: null,
+              },
             },
           },
-        },
-      });
-      instance.herbEaters.push(newObject);
+        });
+        instance.herbEaters.push(newObject);
+      }
+      if (position.button == 1) {
+        await instance.resetGame();
+      }
     });
   }
 
-  resetGame(){
-    this.herbEaters = []
+  async resetGame() {
+    this.herbEaters = [];
+    this.plants = this.initialPlants;
   }
 
   async gameTick() {
     try {
       this.frame();
       await this.update();
+      await this.wigglePlant();
     } catch (error) {
-      console.log("cannot perform tick: ",error);
-      this.resetGame()
+      console.log("cannot perform tick: ", error);
+      this.resetGame();
     }
-   
   }
 
   async update() {
     this.plants = this.plants.filter((plant) => plant.delete == null);
+    this.herbEaters = this.herbEaters.filter((herbeater) => herbeater.delete == null);
     await this.findHerbPaths();
-    await this.move();
+    await this.moveEater();
+    await this.eaterCollision();
   }
-  findHerbPaths() {
+
+  async wigglePlant() {
+    this.plants.forEach((plant) => {
+      const wiggleDirection = Math.floor(Math.random() * 4);
+      const wiggleSpeed = Math.floor(Math.random() * 3);
+      switch (wiggleDirection) {
+        case 0:
+          plant.position.y -= wiggleSpeed;
+          break;
+        case 1:
+          plant.position.y += wiggleSpeed;
+          break;
+        case 2:
+          plant.position.x += wiggleSpeed;
+          break;
+        case 3:
+          plant.position.x -= wiggleSpeed;
+          break;
+
+        default:
+          break;
+      }
+      if (plant.position.x < plant.radius) plant.position.x = plant.radius;
+      if (plant.position.y < plant.radius) plant.position.y = plant.radius;
+
+      if (plant.position.x > 900 - plant.radius)
+        plant.position.x = 900 - plant.radius;
+      if (plant.position.y > 700 - plant.radius)
+        plant.position.y = 700 - plant.radius;
+    });
+  }
+
+  async eaterCollision(){
+    this.herbEaters.forEach((herbEater1,i) => {
+      this.herbEaters.forEach((herbEater2,j) => {
+          if(i!=j){
+            let currDistance = this.calcDistance(
+              herbEater1.position.x,
+              herbEater1.position.y,
+              herbEater2.position.x,
+              herbEater2.position.y,
+            );
+            if(currDistance < (herbEater1.radius + herbEater2.radius)/2){
+              //1 eats 2
+              if(herbEater1.radius >= herbEater2.radius){
+                herbEater1.radius += Math.sqrt(herbEater2.radius);
+                herbEater2.delete=true
+                if (herbEater1.gameProperties.shade < 29) {
+                  herbEater1.gameProperties.shade++;
+                  herbEater1.color =
+                    this.colorShades[herbEater1.gameProperties.shade];
+                }
+              }//2 eats 1
+              else{
+                herbEater2.radius += Math.sqrt(herbEater1.radius);
+                herbEater1.delete=true
+                if (herbEater2.gameProperties.shade < 28) {
+                  herbEater2.gameProperties.shade+=2;
+                  herbEater2.color =
+                    this.colorShades[herbEater2.gameProperties.shade];
+                }
+              }
+            }
+          }
+        })})}
+  async findHerbPaths() {
     this.herbEaters.forEach((herbEater) => {
       let minDistance = 1500;
       let target = null;
@@ -87,11 +198,10 @@ export class GameInstance {
         }
       });
       herbEater.physicalProperty.target.object = target;
-      if(target !=null){
+      if (target != null) {
         herbEater.physicalProperty.target.x = target.position.x;
         herbEater.physicalProperty.target.y = target.position.y;
       }
-      
     });
   }
 
@@ -99,9 +209,9 @@ export class GameInstance {
     return Math.pow((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1), 1 / 2);
   }
 
-  async move() {
+  async moveEater() {
     this.herbEaters.forEach(async (herbEater) => {
-      if (herbEater.physicalProperty.target.object !=null) {
+      if (herbEater.physicalProperty.target.object != null) {
         let delta_X;
         let delta_Y;
         let delta_XY;
@@ -115,8 +225,26 @@ export class GameInstance {
         if (ratio >= 1) {
           herbEater.position.x += delta_X;
           herbEater.position.y += delta_Y;
-            herbEater.physicalProperty.target.object.delete = true;
-            herbEater.physicalProperty.target.object = null;
+          herbEater.physicalProperty.target.object.delete = true;
+          herbEater.physicalProperty.target.object = null;
+          herbEater.gameProperties.eaten++;
+
+          if (
+            herbEater.gameProperties.eaten == 5 ||
+            herbEater.gameProperties.eaten == 10 ||
+            herbEater.gameProperties.eaten == 15
+          ) {
+            if (herbEater.gameProperties.shade < 29) {
+              herbEater.gameProperties.shade++;
+              herbEater.color =
+                this.colorShades[herbEater.gameProperties.shade];
+            }
+          }
+
+          if (herbEater.gameProperties.eaten >= 15) {
+            herbEater.radius++;
+            herbEater.gameProperties.eaten = 0;
+          }
 
           // herbEater.radius++;
         } else {
